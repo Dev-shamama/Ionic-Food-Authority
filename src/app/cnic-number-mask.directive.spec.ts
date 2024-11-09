802da78990fd41d944d44566501d2ec40402376e
Dev-shamama <148217035+Dev-shamama@users.noreply.github.com>
2024-11-09 14:45:07 +0500

    Change angular .json and remove directive for CNIV

--- src/app/cnic-number-mask.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CnicNumberMaskDirective } from './cnic-number-mask.directive';
-
-describe('CnicNumberMaskDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CnicNumberMaskDirective();
-    expect(directive).toBeTruthy();
-  });
-});
