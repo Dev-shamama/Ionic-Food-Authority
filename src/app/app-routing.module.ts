c4a51941d43f397aeff263586e8b1f362f09617f
Dev-shamama <148217035+Dev-shamama@users.noreply.github.com>
2024-11-12 16:47:33 +0500

    Add service and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,12 +17,13 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
   },
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },  {
+  },
+  {
     path: 'licenseform',
     loadChildren: () => import('./licenseform/licenseform.module').then( m => m.LicenseformPageModule)
   },
